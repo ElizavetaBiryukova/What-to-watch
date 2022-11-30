e47a29dacbdc444748d1f7d492d07a6f0a3f3224
Elizaveta Biryukova <birliza@mail.ru>
2022-11-30 15:37:56 +0200

    Adds tabs

--- project/src/components/tabs/tabs-reviews.tsx
+++ project/src/components/tabs/tabs-reviews.tsx
@@ -6,15 +6,15 @@ type ReviewsProps = {
 }
 
 function Reviews({ comments }: ReviewsProps): JSX.Element {
+  const firstColumn: Comments = comments.slice(0, Math.ceil(comments.length / 2));
+  const secondColumn: Comments = comments.slice(-Math.ceil(comments.length / 2));
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {comments.map((comment) => (
-          <Review
-            key={comment.id}
-            comment={comment}
-          />
-        ))}
+        {firstColumn.map((comment) => (<Review comment={comment} key={comment.id} />))}
+      </div>
+      <div className="film-card__reviews-col">
+        {secondColumn.map((comment) => (<Review comment={comment} key={comment.id} />))}
       </div>
     </div>
   );
--- project/src/components/tabs/tabs.tsx
+++ project/src/components/tabs/tabs.tsx
@@ -1,32 +1,55 @@
+import { useState } from 'react';
 import { FilmType, Comments } from '../../types/types';
 import Overview from '../tabs/tabs-overview';
 import Reviews from '../tabs/tabs-reviews';
+import Details from '../tabs/tabs-details';
 
 type TabsProps = {
   film: FilmType;
   comments: Comments;
 }
 
+const titleTabs = [
+  { title: 'Overview' },
+  { title: 'Details' },
+  { title: 'Reviews' },
+];
+
 function Tabs({ film, comments }: TabsProps): JSX.Element {
+  const [activeTab, setActiveTab] = useState(0);
+
+  function getActiveTab(index: number) {
+    switch (index) {
+      case 0:
+        return <Overview film={film} />;
+      case 1:
+        return <Details film={film} />;
+      case 2:
+        return <Reviews comments={comments} />;
+      default:
+        return <Overview film={film} />;
+    }
+  }
+
   return (
 
     <div className="film-card__desc">
       <nav className="film-nav film-card__nav">
         <ul className="film-nav__list">
-          <li className="film-nav__item film-nav__item--active">
-            <a href="/#" className="film-nav__link">Overview</a>
-          </li>
-          <li className="film-nav__item">
-            <a href="/#" className="film-nav__link">Details</a>
-          </li>
-          <li className="film-nav__item">
-            <a href="/#" className="film-nav__link">Reviews</a>
-          </li>
+          {titleTabs.map((titleTab, index) => (
+            <li
+              key={titleTab.title}
+              className={activeTab === index ? 'film-nav__item film-nav__item--active' : 'film-nav__item'}
+              onClick={() => setActiveTab(index)}
+            >
+              <span className="film-nav__link">{titleTab.title}</span>
+            </li>
+          ))}
         </ul>
       </nav>
 
-      <Overview film={film} />
-      <Reviews comments={comments}/>
+      {getActiveTab(activeTab)}
+
     </div>
 
   );
